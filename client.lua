82c1a15e483c18ee349470d5c017f6971d313a47
Peleg <106535297+peleg-development@users.noreply.github.com>
2024-10-18 10:18:05 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,13 +1,4 @@
 local playerPed = PlayerPedId()
-
-
-
--- Simple "machine learning" model parameters
-local modelWeights = {
-    playerSpeedWeight = 0.5,
-    weaponRecoilWeight = 0.3,
-}
-
 local function CalculateAmplitudeScale(num, baseMin, baseMax, targetMin, targetMax)
     return (((num - baseMin) * (targetMax - targetMin)) / (baseMax - baseMin)) + targetMin
 end
@@ -15,17 +6,32 @@ end
 function ApplyRecoilBasedOnStateOrSpeed(ped, weapon, modifier)
     local amplitude = 1.0
 
-    -- Simulating player speed as a feature
-    local pedSpeed = GetEntitySpeed(ped) * 1.5
+    local pedSpeed = GetEntitySpeed(ped)
 
-    -- Simulating weapon recoil as a feature
-    local weaponRecoil = Config.weaponRecoilValues[weapon] or 3.5
+    local weaponData = Config.weaponRecoilValues[weapon] or { recoil = 3.5, type = "generic" }
+    local weaponRecoil = weaponData.recoil
+    local weaponType = weaponData.type
 
-    -- Simulating "machine learning" model decision-making
-    amplitude = CalculateAmplitudeScale(pedSpeed, 0.0, 150.0, 1.0, 8.0) * modelWeights.playerSpeedWeight
-    amplitude = amplitude + CalculateAmplitudeScale(weaponRecoil, 0.0, 10.0, 1.0, 5.0) * modelWeights.weaponRecoilWeight
+    local weaponTypeModifier = Config.weaponTypeModifiers[weaponType] or 1.0
 
-    SetWeaponRecoilShakeAmplitude(weapon, amplitude * modifier)
+    local movementModifier = Config.movementModifiers.still
+    if IsPedRunning(ped) then
+        movementModifier = Config.movementModifiers.running
+    elseif IsPedWalking(ped) then
+        movementModifier = Config.movementModifiers.walking
+    end
+
+    amplitude = CalculateAmplitudeScale(pedSpeed, 0.0, 150.0, 1.0, 8.0) * Config.modelWeights.playerSpeedWeight
+    amplitude = amplitude + CalculateAmplitudeScale(weaponRecoil, 0.0, 10.0, 1.0, 5.0) * Config.modelWeights.weaponRecoilWeight
+
+    -- Adjusting based on aim mode
+    if IsPlayerFreeAiming(PlayerId()) then
+        amplitude = amplitude * Config.modelWeights.aimModeWeight 
+    end
+
+    amplitude = amplitude * weaponTypeModifier * movementModifier * modifier
+
+    SetWeaponRecoilShakeAmplitude(weapon, amplitude)
 end
 
 CreateThread(function()
