bf01496e81bfa5a23c54f2603a3e339d108b8a9e
Peleg <106535297+peleg-development@users.noreply.github.com>
2024-10-18 10:17:31 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,55 +1,31 @@
-local function CalculateAmplitudeScale(num, baseMin, baseMax, targetMin, targetMax)
-    return (((num - baseMin) * (targetMax - targetMin)) / (baseMax - baseMin)) + targetMin
-end
+Config = {}
 
--- Function to calculate and apply recoil based on multiple conditions
-function ApplyRecoilBasedOnStateOrSpeed(ped, weapon, modifier)
-    local amplitude = 1.0
+-- Configurable weights and options
+Config.modelWeights = {
+    playerSpeedWeight = 0.5,   -- Default speed weight
+    weaponRecoilWeight = 0.3,  -- Default weapon recoil weight
+    aimModeWeight = 0.2,       -- Recoil adjustment for aiming modes (hipfire vs ADS)
+}
 
-    -- Simulating player speed as a feature
-    local pedSpeed = GetEntitySpeed(ped)
+-- Configurable weapon recoil values
+Config.weaponRecoilValues = {
+    [`weapon_pistol50`] = { recoil = 3.5, type = "pistol" },
+    [`weapon_assaultrifle`] = { recoil = 5.0, type = "rifle" },
+    -- Add more weapons here with their recoil and types
+    -- [`new_weapon`] = { recoil = new_recoil_value, type = "weapon_type" },
+}
 
-    -- Get the weapon recoil value and type
-    local weaponData = Config.weaponRecoilValues[weapon] or { recoil = 3.5, type = "generic" }
-    local weaponRecoil = weaponData.recoil
-    local weaponType = weaponData.type
+-- Weapon type-based modifiers (can be tuned for realism)
+Config.weaponTypeModifiers = {
+    pistol = 1.0,
+    rifle = 1.5,
+    shotgun = 2.0,
+    smg = 1.2,
+}
 
-    -- Get weapon type modifier
-    local weaponTypeModifier = Config.weaponTypeModifiers[weaponType] or 1.0
-
-    -- Calculate movement-based recoil modifier
-    local movementModifier = Config.movementModifiers.still
-    if IsPedRunning(ped) then
-        movementModifier = Config.movementModifiers.running
-    elseif IsPedWalking(ped) then
-        movementModifier = Config.movementModifiers.walking
-    end
-
-    -- Simulating "machine learning" model decision-making
-    amplitude = CalculateAmplitudeScale(pedSpeed, 0.0, 150.0, 1.0, 8.0) * Config.modelWeights.playerSpeedWeight
-    amplitude = amplitude + CalculateAmplitudeScale(weaponRecoil, 0.0, 10.0, 1.0, 5.0) * Config.modelWeights.weaponRecoilWeight
-
-    -- Adjusting based on aim mode
-    if IsPlayerFreeAiming(PlayerId()) then
-        amplitude = amplitude * Config.modelWeights.aimModeWeight -- Less recoil while aiming
-    end
-
-    -- Final adjustment with weapon type modifier and movement modifier
-    amplitude = amplitude * weaponTypeModifier * movementModifier * modifier
-
-    -- Apply the calculated recoil amplitude
-    SetWeaponRecoilShakeAmplitude(weapon, amplitude)
-end
-
--- Thread to continuously apply the recoil system
-CreateThread(function()
-    while true do
-        Wait(150)
-
-        local ped = PlayerPedId()
-        if IsPedArmed(ped, 6) then
-            local _, currentWeapon = GetCurrentPedWeapon(ped, true)
-            ApplyRecoilBasedOnStateOrSpeed(ped, currentWeapon, 1.0)
-        end
-    end
-end)
+-- Movement-based recoil modifiers
+Config.movementModifiers = {
+    still = 0.8,
+    walking = 1.0,
+    running = 1.2,
+}
